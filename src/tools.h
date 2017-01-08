85b212087b45c0dbbb3f6165660f367cc6152874
Dominik Neise <neised@phys.ethz.ch>
2017-01-08 18:08:23 +0100

    add functions for integer statistics on ADC pin measurements

--- src/tools.h
+++ src/tools.h
@@ -9,9 +9,44 @@ namespace tools
     int availableMemory();
 
     struct mean_std_t {
-      double mean;
-      double std;
+      uint32_t mean;
+      uint32_t var;
     };
+
+    mean_std_t get_mean_std(const int pin, const uint16_t n)
+    {
+        mean_std_t tmp;
+        tmp.mean = 0;
+        tmp.var = 0;
+        if (n == 0) return tmp;
+
+        uint64_t sum = 0;
+        uint64_t sq_sum = 0;
+        for (uint16_t i=0; i < n; i++)
+        {
+            uint16_t a = analogRead(pin);
+            sum += a;
+            sq_sum += a * a;
+        }
+
+        uint64_t N = n;
+        tmp.mean = (uint32_t) (sum / n);
+        tmp.var = (N * sq_sum - sum * sum) / (N * N);
+        return tmp;
+    }
+
+    uint32_t get_mean(const int pin, const uint16_t n)
+    {
+        if (n == 0) return 0;
+        uint32_t sum = 0;
+        for (uint16_t i=0; i < n; i++)
+        {
+            uint16_t a = analogRead(pin);
+            sum += a;
+        }
+        return sum / n;
+    }
+
 };
 
 #endif // tools_h
