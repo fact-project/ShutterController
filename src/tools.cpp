05e7686214e489ae6164fff20712fbf607a40686
Dominik Neise <neised@phys.ethz.ch>
2017-01-08 18:40:37 +0100

    make it compile

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,3 +9,37 @@ int tools::availableMemory() {
 
   return free_memory;
 }
+
+uint32_t tools::get_mean(const int pin, const uint16_t n)
+{
+    if (n == 0) return 0;
+    uint32_t sum = 0;
+    for (uint16_t i=0; i < n; i++)
+    {
+        uint16_t a = analogRead(pin);
+        sum += a;
+    }
+    return sum / n;
+}
+
+tools::mean_std_t tools::get_mean_std(const int pin, const uint16_t n)
+{
+    mean_std_t tmp;
+    tmp.mean = 0;
+    tmp.var = 0;
+    if (n == 0) return tmp;
+
+    uint64_t sum = 0;
+    uint64_t sq_sum = 0;
+    for (uint16_t i=0; i < n; i++)
+    {
+        uint16_t a = analogRead(pin);
+        sum += a;
+        sq_sum += a * a;
+    }
+
+    uint64_t N = n;
+    tmp.mean = (uint32_t) (sum / n);
+    tmp.var = (N * sq_sum - sum * sum) / (N * N);
+    return tmp;
+}
--- src/tools.h
+++ src/tools.h
@@ -16,40 +16,8 @@ namespace tools
       uint32_t var;
     };
 
-    mean_std_t get_mean_std(const int pin, const uint16_t n)
-    {
-        mean_std_t tmp;
-        tmp.mean = 0;
-        tmp.var = 0;
-        if (n == 0) return tmp;
-
-        uint64_t sum = 0;
-        uint64_t sq_sum = 0;
-        for (uint16_t i=0; i < n; i++)
-        {
-            uint16_t a = analogRead(pin);
-            sum += a;
-            sq_sum += a * a;
-        }
-
-        uint64_t N = n;
-        tmp.mean = (uint32_t) (sum / n);
-        tmp.var = (N * sq_sum - sum * sum) / (N * N);
-        return tmp;
-    }
-
-    uint32_t get_mean(const int pin, const uint16_t n)
-    {
-        if (n == 0) return 0;
-        uint32_t sum = 0;
-        for (uint16_t i=0; i < n; i++)
-        {
-            uint16_t a = analogRead(pin);
-            sum += a;
-        }
-        return sum / n;
-    }
-
+    uint32_t get_mean(const int pin, const uint16_t n);
+    mean_std_t get_mean_std(const int pin, const uint16_t n);
 };
 
 #endif // tools_h
