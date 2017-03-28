d8897ec5b92b1f07a48731044a848bad15a15345
Dominik Neise <neised@phys.ethz.ch>
2017-03-28 18:00:02 +0200

    Revert "use ArduinoJson to emit human+machine readable output"
    This reverts commit be98da1e5efdf049cc52f8fc0f8bc8898c2e181b.

--- src/ShutterController.cpp
+++ src/ShutterController.cpp
@@ -1,13 +1,9 @@
-/* Explain what is going on with Timer0.
-*/
-
 #include "tools.h"
 #include "DualVNH5019MotorShield.h"
 #include "LinakHallSensor.h"
 #include <stdio.h> // for function sprintf
 #include <SPI.h>
 #include <Ethernet.h>
-#include <ArduinoJson.h>
 
 byte _mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x5C, 0x91 };
 IPAddress _ip(10,0,100,36);
@@ -57,55 +53,51 @@ bool is_user_command_new = false;
 const unsigned long DRIVE_TIME_LIMIT_MS = 15000UL * TIMER0_MESS_UP_FACTOR;
 
 void print_system_state(){
-    StaticJsonBuffer<100> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-    root["state_number"] = system_state;
+    server.print("Current state: ");
     switch (system_state){
-        case S_OPEN: root["state_name"] = "Open"; break;
-        case S_DRIVE_OPENING: root["state_name"] = "Opening"; break;
-        case S_FAIL_OPEN: root["state_name"] = "Fail uring Opening"; break;
-        case S_CLOSED: root["state_name"] = "Closed"; break;
-        case S_DRIVE_CLOSING: root["state_name"] = "Closing"; break;
-        case S_FAIL_CLOSE: root["state_name"] = "Fail uring Closing"; break;
-        case S_UNKNOWN: root["state_name"] = "Unknown"; break;
+        case S_OPEN: server.println("Open"); break;
+        case S_DRIVE_OPENING: server.println("Opening"); break;
+        case S_FAIL_OPEN: server.println("Fail during Opening"); break;
+        case S_CLOSED: server.println("Closed"); break;
+        case S_DRIVE_CLOSING: server.println("Closing"); break;
+        case S_FAIL_CLOSE: server.println("Fail during Closing"); break;
+        case S_UNKNOWN: server.println("Unknown"); break;
         default:
-            root["state_name"] = "Must never happen!";
+            server.println("Must never happen!");
     }
-    root.printTo(server);
 }
 
 void print_motor_stop_reason(motor_stop_reason_t r){
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-
-    root.printTo(server);
+    server.print("Motor Stop Reason: ");
+    switch (r){
+        case M_TIMEOUT: server.println("Timeout"); break;
+        case M_OVERCURRENT: server.println("Overcurrent"); break;
+        case M_ZEROCURRENT: server.println("Zerocurrent/Endswitch"); break;
+        case M_POSITION_REACHED: server.println("Position Reached"); break;
+        case M_NO_REASON: server.println("No Reason! bug!!!"); break;
+        case M_USER_INTERUPT: server.println("User Interupt"); break;
+        default:
+            server.println("Must never happen!");
+    }
 }
 
-void report_motor_info(int motor, unsigned long duration, motor_stop_reason_t reason) {
-    StaticJsonBuffer<1000> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-    root["motor_id"] = motor;
-    root["duration"] = duration / TIMER0_MESS_UP_FACTOR;
-    root["motor_stop_reason_number"] = reason;
-    switch (reason){
-        case M_TIMEOUT: root["motor_stop_reason_name"] = "Timeout"; break;
-        case M_OVERCURRENT: root["motor_stop_reason_name"] = "Overcurrent"; break;
-        case M_ZEROCURRENT: root["motor_stop_reason_name"] = "Zerocurrent/Endswitch"; break;
-        case M_POSITION_REACHED: root["motor_stop_reason_name"] = "Position Reached"; break;
-        case M_NO_REASON: root["motor_stop_reason_name"] = "No Reason! bug!!!"; break;
-        case M_USER_INTERUPT: root["motor_stop_reason_name"] = "User Interupt"; break;
-        default:
-            root["motor_stop_reason_name"] = "Must never happen!";
-    }
 
-    JsonArray& current = root.createNestedArray("current");
-    JsonArray& position = root.createNestedArray("position");
+void report_motor_info(int motor, unsigned long duration, motor_stop_reason_t reason) {
+
+    server.print("info about motor: ");
+    server.println(motor);
+    server.print("duration[ms]: ");
+    server.println(duration / TIMER0_MESS_UP_FACTOR);
+    print_motor_stop_reason(reason);
+    server.print("current and positions: #");
+    server.println(archive_pointer);
     for (uint16_t i=0; i<archive_pointer; i++) {
-        current.add(archive[i].current);
-        position.add(archive[i].position);
+        server.print(archive[i].current);
+        server.print(' ');
+        server.print(archive[i].position);
+        server.println();
     }
     archive_pointer = 0;
-    root.printTo(server);
 }
 
 void fetch_new_command()
@@ -176,27 +168,19 @@ bool close_upper() { return move_fully_supervised(1, false); }
 bool open_lower() { return move_fully_supervised(0, true); }
 bool open_upper() { return move_fully_supervised(1, true); }
 
-void ack(char cmd, bool ok) {
-    StaticJsonBuffer<100> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-    root["ok"] = ok;
-    root["message"] = ok ? "command valid" : "command ignored";
-    root["last_command"] = cmd;
-    root.printTo(server);
-}
-
 void acknowledge_no_operation(char cmd)
 {
-    ack(cmd, false);
+    server.print("Ignoring command: ");
+    server.println(cmd);
     print_system_state();
 }
 
 void init_drive_close(char cmd)
 {
     system_state = S_DRIVE_CLOSING;
-    ack(cmd, true);
+    server.print("Command Valid: ");
+    server.println(cmd);
     print_system_state();
-
     if (close_lower() == false) {
         system_state = S_FAIL_CLOSE;
         return;
@@ -212,9 +196,9 @@ void init_drive_close(char cmd)
 void init_drive_open(char cmd)
 {
     system_state = S_DRIVE_OPENING;
-    ack(cmd, true);
+    server.print("Command Valid: ");
+    server.println(cmd);
     print_system_state();
-
     if (open_upper() == false){
         system_state = S_FAIL_OPEN;
         return;
