30fca0d69847ffe3061c531d5a603f0c2e638d5b
Dominik Neise <neised@phys.ethz.ch>
2021-09-22 22:49:57 +0200

    bugfix

--- src/ShutterController.cpp
+++ src/ShutterController.cpp
@@ -134,11 +134,13 @@ void check_motor_current(){
 
 void send_status_human_readable(char foo)
 {
-  char formatted_string[150];
+  char formatted_string[200];
   for (int i=0; i<2; i++){
     tools::mean_std_t current = md.get_mean_std(i, 300);
     tools::mean_std_t position = lh.get_mean_std(i, 300);
-    sprintf(formatted_string+i*70, "M%1d: I=%4ld+-%4ld pos=%4ld+-%4ld S=%3d cmd=%c system_state=%d  %c     ",
+    //M0: I=   0+-   0 pos= 751+--2147483648 S=  0 cmd=x system_state=4  |  M1: I=   0+-   0 pos=  15+-   1 S=  0 cmd=x system_state=4  |
+    //M0: I=   0+-   0 pos= 751+-   1 S=  0 cmd=x system_state=4  |
+    sprintf(formatted_string+i*70, "M%1d: I=%4ld+-%4ld pos=%4ld+-%4ld S=%3d cmd=%c system_state=%d  %c               ",
       i, // motor id
       (long)current.mean, (long)current.std,
       (long)position.mean, (long)position.std,
